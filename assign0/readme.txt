b20f28f310d1085f0b9aa66b619d036e4dca8305
Yao Xiao <yaox4@myth65.stanford.edu>
2018-05-17 21:30:11 -0700

    [assign0] init commit

---
+++ assign0/readme.txt
@@ -0,0 +1,15 @@
+File: readme.txt
+Assignment: assign0
+Author: YOUR NAME HERE
+----------------------
+
+1. Please initial the items below to confirm your course enrollment.
+
+[ ] I will attend the CS107 midterm and final as scheduled and I understand no alternate/makeup exams will be offered. The midterm is Fri May 11th in class, the final is Wed June 13th 12:15-3:15pm. (Remote SCPD students take exams on-site on the scheduled date).
+[ ] I have read and understand the CS107 collaboration policy http://cs107.stanford.edu/collaboration.html. By adding my initials, I affirm my intention to abide by the policy.
+
+
+INTRUDER DETECTION ACTIVITY
+2a)
+2b)
+2c)
---
+++ assign0/triangle.c
@@ -0,0 +1,33 @@
+/* File: triangle.c
+ * ----------------
+ * Outputs an ascii representation of the Sierpinski triangle.
+ *
+ * The pattern of stars is generated using a bitwise AND 
+ * on the coordinates. This works due to the relationship
+ * between the fractal and Pascal's triangle.
+ * https://en.wikipedia.org/wiki/Sierpinski_triangle
+ */
+
+#include <stdio.h>
+
+void print_triangle(int nlevels)
+{
+    int size = 1 << nlevels;
+
+    for (int row = size - 1; row >= 0; row--) {
+        for (int col = 0; col < row; col++) {
+            printf(" ");
+        }
+        for (int col = 0; col + row < size; col++) {
+            printf("%c ", (col & row) ? ' ' : '*');
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int nlevels = 3;
+    print_triangle(nlevels);
+    return 0;
+}
