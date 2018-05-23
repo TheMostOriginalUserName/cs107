b148c006ec35ac5e132c8f1b47a7f9f007b4e33c
yaoforx <yaoforx@gmail.com>
2018-05-23 01:33:09 -0700

    assign2 passed sanitycheck

--- assign2/mywhich.c
+++ assign2/mywhich.c
@@ -2,12 +2,16 @@
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
-
-
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 int main(int argc, char *argv[], const char *envp[])
 {
     const char *searchpath = get_env_value(envp, "MYPATH");
     if (searchpath == NULL) searchpath = get_env_value(envp, "PATH");
+   // printf("PATH: %s\n", searchpath);
+   // printf("argc: %d\n", argc);
 
     if (argc == 1) {
         char dir[PATH_MAX];
@@ -17,6 +21,29 @@ int main(int argc, char *argv[], const char *envp[])
         while (scan_token(&remaining, ":", dir, sizeof(dir))) {
             printf("%s\n", dir);
         }
+    } else {
+	for(int i = 1; i < argc; i++){
+            char dir[PATH_MAX];
+            const char* path = searchpath;		
+	    while(scan_token(&path,":",dir,sizeof(dir))){
+		struct dirent *de;
+		DIR *dr = opendir(dir);
+		if(dr == NULL) continue;
+		    while((de = readdir(dr)) != NULL) {
+				
+			if(strcmp(de->d_name,argv[i]) == 0){
+				
+			    if(!strcmp(dir,".")|| !strcmp(de->d_name,".")||! strcmp(de->d_name,"..")) continue;
+			      strcat(dir,"/");
+			      printf("%s\n",strcat(dir,argv[i]));
+			      goto flag;	
+			}
+			}
+			
+			closedir(dr);
+		}	
+		flag:;
+	}
     }
     return 0;
 }
--- assign2/util.c
+++ assign2/util.c
@@ -4,11 +4,50 @@
 
 const char *get_env_value(const char *envp[], const char *key)
 {
-   return NULL;
+  	size_t size = 0;
+         const char * move = key;
+
+	while(*move != '\0') {
+		move++;
+		size++;
+	}
+	
+	int lon = 0;
+	while(envp[++lon] != '\0');
+	
+	for(int i = 0; i < lon; i++) {
+		int index = 0;
+		while(envp[i][++index] != '=');
+		if(strncmp(key,envp[i],index) == 0) {
+			return (envp[i] + index + 1);
+		}
+	}
+	return NULL;
+	
 }
 
 
 bool scan_token(const char **p_input, const char *delimiters, char buf[], size_t buflen)
 {
-    return false;
+
+	const char * start;
+	start  = *p_input;
+	
+	int end = 0;
+	if(*start == '\0') return false;	
+	int i = 0;
+	end = strcspn(start,delimiters);
+	while(end == 0 && *start != '\0'){
+		end = strcspn(++start,delimiters);
+	}
+	if(*start == '\0') return false;
+	while( i != end  && strlen(buf) != buflen - 1){
+		buf[i] = *start;
+		i++;
+		start++;
+	}
+	*p_input = start;
+	buf[i] = '\0';
+	
+    return true;
 }
